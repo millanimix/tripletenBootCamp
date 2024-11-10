136fee8712c7c96d926ea52cd366166f067cfb10
Leonardo Israel Millan Garcia <millan.galeis@gmail.com>
2024-11-09 18:26:31 -0600

    Print function excercise

--- 01_Sprint1/02_variables.py
+++ 01_Sprint1/02_variables.py
@@ -1,5 +1,16 @@
 # Practice file S1
-1 + 1
-print(1+1)
 
-print ('Hello')
+print ('¡Hola, mundo!')
+
+print('El número favorito de Max es...')
+print()
+print(3)
+
+emperor = 'Romulus Augustulus'
+reign_start = 475
+
+print(emperor)
+print(reign_start)
+
+print(emperor, reign_start)
+
