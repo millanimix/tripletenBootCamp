2c4402cbd586e0edce7112a26b50a61e8c8b4f3b
Leonardo Israel Millan Garcia <drbiggorila@drbiggorillaAir.local>
2024-11-12 23:28:30 -0600

    Strings and lenght

--- 01_Sprint1/03_String.py
+++ 01_Sprint1/03_String.py
@@ -1,1 +1,37 @@
-# Practice file S1
+# Chapter 3. Strings
+
+my_string_1 = '¡String 1, string 2, string rojo, string, azul!'
+my_string_2 = "¡String 1, string 2, string rojo, string, azul!"
+
+print(my_string_1)
+print(my_string_2)
+
+# Usar dobles para imprimir apostrofe
+print("Let's learn about strings!")
+
+# Caráceres de escape
+print('Let\'s learn about strings!')
+
+print('Pete\'s favorite movie is:\t\t the last one he watches\n:D')
+
+shopping_list = '''Mi lista de compras:
+2 cartones de leche,
+cereales de desayuno,
+huevos,
+pan'''
+
+print(shopping_list)
+
+sonnet = "Que la ruda mano del invierno no deshaga,\nEn ti el verano, antes tú debes ser destilado:\nEndulza algún pomo, en algún lugar atesórate\nCon el tesoro de la belleza antes que ella a sí se mate.\nNo es usura prohibida hacer pagar,\nA quien feliz paga por lo que debe;\nEso sería para ti engendrar otro tú,\nO diez veces más feliz, sea un diez por uno;\nDiez veces más feliz serás de lo que eres,\nSi diez de ti diez veces te figuran:\nPues, ¿qué podría hacer la muerte si partes,\nDejándote vivo, con descendencia?\nNo seas egoísta, tu belleza es mucha para conquista\nDe la muerte, que a los gusanos tus herederos haría."
+print(sonnet)
+print()
+print(len(sonnet))
+
+string_1 = 'HolaAdiós'
+string_2 = 'Hola Adiós'
+string_3 = 'Hola\nAdiós'
+print(len(string_1))
+print(len(string_2))
+print(len(string_3))
+
+print(len(''))
