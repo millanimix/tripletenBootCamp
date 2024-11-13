a5dbd42b0959511882340336b66988855b343d63
Leonardo Israel Millan Garcia <drbiggorila@drbiggorillaAir.local>
2024-11-12 21:41:28 -0600

    Sprint 1, Cap 2, Lesson 2. Operators practice

--- 01_Sprint1/02_variables.py
+++ 01_Sprint1/02_variables.py
@@ -14,3 +14,27 @@ print(reign_start)
 
 print(emperor, reign_start)
 
+# Operators practice 
+# suma
+print(1.5 + 10)
+# Resta
+print(1.5 - 10)
+# Multiplcacion
+print(1.5 * 10)
+# exponente
+print(1.5 ** 10)
+# Division
+print(1.5 / 10)
+# Cociente
+print(15 // 10)
+# Modulo
+print(15 % 10)
+
+print( (2 ** 3 + 12 / 2) / (5 * 4 -13))
+
+french_speakers = 284.9
+german_speakers = 132.0
+
+total_speakers = french_speakers + german_speakers
+
+print('Millones de personas que hablan francés o alemán:', total_speakers)
