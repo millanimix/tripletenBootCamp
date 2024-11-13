809b8b687d88567bd9c222e04a889ca1fe95953e
Leonardo Israel Millan Garcia <drbiggorila@drbiggorillaAir.local>
2024-11-12 22:20:02 -0600

    Data types and type convertion

--- 01_Sprint1/02_variables.py
+++ 01_Sprint1/02_variables.py
@@ -38,3 +38,51 @@ german_speakers = 132.0
 total_speakers = french_speakers + german_speakers
 
 print('Millones de personas que hablan francés o alemán:', total_speakers)
+
+# Data types practice
+language = 'Chino'
+print(type(language))
+
+# Strings
+ch_language = 'Chino'
+en_language = 'Inglés'
+
+# Flotantes
+ch_website_share = 0.017
+en_website_share = 0.539
+
+# Entero
+ch_total_speakers = 1107
+en_total_speakers = 1121
+
+# Booleanos
+ch_majority_native = True
+en_majority_native = False
+
+# Operador suma
+print(ch_language + en_language)
+print(ch_website_share + en_website_share)
+print(ch_total_speakers + en_total_speakers)
+print(ch_majority_native + en_majority_native)
+
+# Se pueden multiplicar pero no sumar
+print(' 123ABC ' * 6)
+
+#Conversión de tipos de datos
+total_sites = 10000000
+
+en_sites = total_sites * en_website_share
+en_sites = int(en_sites)
+
+print(en_sites)
+print(type(en_sites))
+
+ch_total_speakers = int(ch_total_speakers)
+ch_total_speakers = ch_total_speakers + 10
+print(ch_total_speakers)
+print(type(ch_total_speakers))
+
+zipcode = 90210
+print(type(zipcode))
+zipcode = str(zipcode)
+print(type(zipcode))
