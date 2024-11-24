e73e953812bcd17d8870cf40ecb369d30ed33b84
Leonardo Israel Millan Garcia <drbiggorila@drbiggorillaAir.local>
2024-11-23 19:02:19 -0600

    Augmented Assigment & Built-in Functions

---
+++ 02_Spirnt2/01_Bucles.py
@@ -0,0 +1,40 @@
+# Bucles
+
+fr_speakers = 284.9
+fr_speakers = 284.9 + 2.5
+print(fr_speakers)
+
+fr_speakers = 284.9
+fr_speakers = fr_speakers + 2.5
+print(fr_speakers)
+
+# Asignación auentada / Augmented Assigment
+fr_speakers = 284.9
+fr_speakers += 2.5
+print(fr_speakers)
+
+items = 10
+
+items += 5 # igual que items = items + 5
+print(items)
+
+items -= 5 # igual que items = items - 5
+print(items)
+
+items *= 5 # igual que items = items * 5
+print(items)
+
+items /= 5 # igual que items = items / 5
+print(items)
+
+# Funciones integradas / Built-in Functions
+movies_duration = [142, 175, 152, 195, 201, 154, 178, 139, 133, 126]
+
+total_duration = sum(movies_duration) # suma todos los elementos de la lista
+max_duration = max(movies_duration)
+min_duration = min(movies_duration)
+
+print(total_duration)
+print(max_duration)
+print(min_duration)
+
--- 02_Spirnt2/practice.py
+++
@@ -1,2 +0,0 @@
-# Practice file S2
-
