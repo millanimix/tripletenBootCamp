e3834d7b8c257fbe4d21cd5229603757d2b1c427
Leonardo Israel Millan Garcia <drbiggorila@drbiggorillaAir.local>
2024-12-01 23:42:33 -0600

    Filtering with logical operators

--- 02_Spirnt2/02_Sentencias_Condicionales.py
+++ 02_Spirnt2/02_Sentencias_Condicionales.py
@@ -174,3 +174,31 @@ for movie in movies_info_2:
 for movie in movies_filtered_2:
     print(movie)
 
+# Operadores lógicos y filtros
+# Filtering with logical operators
+
+print()
+print('Filtering tables')
+print()
+
+movies_info = [
+    ['The Shawshank Redemption', 'USA', 1994, 'drama', 142, 9.111],
+    ['The Godfather', 'USA', 1972, 'drama, crime', 175, 8.730],
+    ['The Dark Knight', 'USA', 2008, 'fantasy, action, thriller', 152, 8.499],
+    ["Schindler's List", 'USA', 1993, 'drama', 195, 8.818],
+    ['The Lord of the Rings: The Return of the King', 'New Zealand', 2003, 'fantasy, adventure, drama', 201, 8.625],
+    ['Pulp Fiction', 'USA', 1994, 'thriller, comedy, crime', 154, 8.619],
+    ['The Good, the Bad and the Ugly', 'Italy', 1966, 'western', 178, 8.521],
+    ['Fight Club', 'USA', 1999, 'thriller, drama, crime', 139, 8.644],
+    ['Harakiri', 'Japan', 1962, 'drama, action, history', 133, 8.106],
+    ['Good Will Hunting', 'USA', 1997, 'drama, romance', 126, 8.077]
+]
+
+movies_filtered_3 = []
+
+for movie in movies_info:
+    if movie[2] > 1990 and movie[1] == 'USA':
+        movies_filtered_3.append(movie)
+
+for movie in movies_filtered_3:
+    print(movie)
