63b30036a4bff066c8b9002e72675b90400f37cf
Leonardo Israel Millan Garcia <drbiggorila@drbiggorillaAir.local>
2024-12-01 22:53:01 -0600

    elif and else statements

--- 02_Spirnt2/02_Sentencias_Condicionales.py
+++ 02_Spirnt2/02_Sentencias_Condicionales.py
@@ -51,3 +51,47 @@ if 'ogres' in quote:
 else:
     print('¡Aquí no, pequeño bromista!')
 
+# else and elif
+
+weather = 'lluvia'
+
+if weather == 'lluvia':
+    item_to_take = 'paraguas'
+if weather == 'sol':
+    item_to_take = 'sombrero'
+if weather == 'nieve':
+    item_to_take = 'gorro y bufanda'
+else:
+    item_to_take = 'nada'
+
+print(weather)
+print(item_to_take)
+
+weather = 'lluvia'
+
+if weather == 'lluvia':
+    item_to_take = 'paraguas'
+elif weather == 'sol':
+    item_to_take = 'sombrero'
+elif weather == 'nieve':
+    item_to_take = 'gorro y bufanda'
+else:
+    item_to_take = 'nada'
+
+print(weather)
+print(item_to_take)
+
+years = [1994, 1972, 2008, 1993, 2003, 1994, 1966, 1999, 1962, 1997]
+for year in years:
+    if 1960 <= year <= 1969:
+        print('La pelicula se estrenó en la decada de 1960.')
+    elif 1970 <= year <= 1979:
+        print('La pelicula se estrenó en la decada de 1970.')
+    elif 1980 <= year <= 1989:
+        print('La pelicula se estrenó en la decada de 1980.')
+    elif 1990 <= year <= 1999:
+        print('La pelicula se estrenó en la decada de 1990.')
+    elif 2000 <= year <= 2009:
+        print('La pelicula se estrenó en la decada de 2000.')
+    else:
+        print('Tiempo no definido')
