58fb7e44e618237bb058d1253c4bbd22d7c57941
Dr. Big Gorilla <drbiggorila@drbiggorillaAir.local>
2024-12-09 03:39:06 -0600

    Project Exercises

--- 02_Spirnt2/04_Funciones.py
+++ 02_Spirnt2/04_Funciones.py
@@ -308,3 +308,255 @@ def subtraction(minuend, subtrahend):
 # el argumento posicional. A continuación, puedes especificar el argumento con nombre.
 # Por lo tanto, una llamada correcta a la función tiene el siguiente formato: 
 subtraction(5, subtrahend=6)
+
+
+clients = [
+    [32456, "Jack Wilson", 32, 150000, "Healthcare"],
+    [34591, "Nina Brown", 45, 250000, "Telecom"],
+    [37512, "Alex Smith", 39, 210000, "IT"],
+    [39591, "Brian Perez", 29, 340000, "Transportation"],
+    [45123, "Sarah Lee", 28, 120000, "Marketing"],
+    [47635, "David Kim", 36, 180000, "Finance"],
+    [49571, "Samantha Chen", 42, 220000, "Retail"],
+    [50391, "Juan Rodriguez", 31, 160000, "Architecture"],
+    [34556, "Lucas Hernandez", 37, 75000, "Education"],
+    [64291, "Jessica Li", 25, 125000, "IT"],
+    [74512, "Emma Davis", 47, 197000, "Finance"],
+    [83191, "Sophia Perez", 34, 225000, "Transportation"],
+    [91023, "Liam Kim", 29, 98000, "Retail"],
+    [96435, "Ava Chen", 31, 175000, "Marketing"],
+    [100571, "Noah Rodriguez", 28, 85000, "Architecture"],
+    [101321, "Olivia Wilson", 44, 310000, "Telecom"],
+    [104556, "William Brown", 38, 289000, "Finance"],
+    [105491, "Emily Smith", 29, 193000, "Healthcare"],
+    [107512, "Michael Perez", 53, 415000, "Transportation"]
+]
+
+
+# crea aquí tu función filter_clients
+def filter_clients(data, area):
+    new_list = []
+    for client in data:
+        if client[4] == area:
+            new_list.append(client)
+    return new_list
+
+filtered_list  = filter_clients(clients, 'Transportation') # llama a la función aquí
+
+# muestra el resultado aquí
+print(filtered_list)
+
+# define tu función aquí
+def clean_user(user_info, index_name, index_age):
+    # Paso 1: elimina del nombre espacios iniciales y finales, así como guiones
+    user_name_1 = user_info[index_name].strip()
+    user_name_1 = user_name_1.replace('_', ' ')
+
+    # Paso 2: convierte la edad en entero
+    user_age_1 = int(user_info[index_age])
+
+    # Paso 3: separa el nombre y el apellido en una sublista
+    user_name_1 = user_name_1.split()
+
+    # Prepara la lista con la información completa del usuario
+    # Reemplaza el nombre y la edad originales con los datos limpios
+    user_info[index_name] = user_name_1
+    user_info[index_age] = user_age_1
+
+    return user_info
+
+# Prueba la función
+test_user = ['32415', ' mike_reed ', 32.0, ['ELECTRONICS', 'SPORT', 'BOOKS'], [894, 213, 173]]
+name_index = 1
+age_index = 2
+
+print(clean_user(test_user, name_index, age_index)) # completa aquí el llamado de la función
+
+print()
+print()
+
+users = [
+    ['32415', ' mike_reed ', 32.0, ['ELECTRONICS', 'SPORT', 'BOOKS'], [894, 213, 173]],
+    ['31980', 'kate morgan', 24.0, ['CLOTHES', 'BOOKS'], [439, 390]],
+    ['32156', ' john doe ', 37.0, ['ELECTRONICS', 'HOME', 'FOOD'], [459, 120, 99]],
+    ['32761', 'SAMANTHA SMITH', 29.0, ['CLOTHES', 'ELECTRONICS', 'BEAUTY'], [299, 679, 85]],
+    ['32984', 'David White', 41.0, ['BOOKS', 'HOME', 'SPORT'], [234, 329, 243]],
+    ['33001', 'emily brown', 26.0, ['BEAUTY', 'HOME', 'FOOD'], [213, 659, 79]],
+    ['33767', ' Maria Garcia', 33.0, ['CLOTHES', 'FOOD', 'BEAUTY'], [499, 189, 63]],
+    ['33912', 'JOSE MARTINEZ', 22.0, ['SPORT', 'ELECTRONICS', 'HOME'], [259, 549, 109]],
+    ['34009', 'lisa wilson ', 35.0, ['HOME', 'BOOKS', 'CLOTHES'], [329, 189, 329]],
+    ['34278', 'James Lee', 28.0, ['BEAUTY', 'CLOTHES', 'ELECTRONICS'], [189, 299, 579]],
+]
+
+users_categories_low = []
+for user in users:
+    categories_low = []
+    for category in user[3]: # escribe tu código aquí
+        lowered_category = category.lower() # escribe tu código aquí
+		# escribe tu código aquí
+        categories_low.append(lowered_category)
+    user.pop(3)
+    user.insert(3, categories_low)
+    users_categories_low.append(user)
+
+# escribe tu código aquí
+print(users_categories_low)
+
+
+print()
+print()
+
+def clean_user(user_info, name_index, age_index, cat_index):
+
+  # Paso 1: pon todo en minúsculas y elimina del nombre espacios iniciales y finales, así como guiones
+  user_name_1 = user_info[name_index].lower()
+  user_name_1 = user_name_1.strip()
+  user_name_1 = user_name_1.replace('_', ' ')
+
+  # Paso 2: convierte la edad en entero
+  user_age_1 = int(user_info[age_index]) # escribe tu código aquí
+
+  # Paso 3: separa el nombre y el apellido en una sublista
+  user_name_1 = user_name_1.split() # escribe tu código aquí
+
+  # Paso 4: separa el nombre y el apellido en una sublista
+  categories_low = []
+  for category in user_info[cat_index]: # escribe tu código aquí
+      lowered_category = category.lower()
+      categories_low.append(lowered_category)
+
+  # Prepara la lista con la información completa del usuario
+  # Reemplaza el nombre y la edad originales con los datos limpios
+  # escribe tu código aquí
+  user_info[name_index] = user_name_1
+  user_info[age_index] = user_age_1
+  user_info[cat_index] = categories_low
+
+  return user_info
+
+
+users = [
+    ['32415', ' mike_reed ', 32.0, ['ELECTRONICS', 'SPORT', 'BOOKS'], [894, 213, 173]],
+    ['31980', 'kate morgan', 24.0, ['CLOTHES', 'BOOKS'], [439, 390]],
+    ['32156', ' john doe ', 37.0, ['ELECTRONICS', 'HOME', 'FOOD'], [459, 120, 99]],
+    ['32761', 'SAMANTHA SMITH', 29.0, ['CLOTHES', 'ELECTRONICS', 'BEAUTY'], [299, 679, 85]],
+    ['32984', 'David White', 41.0, ['BOOKS', 'HOME', 'SPORT'], [234, 329, 243]],
+    ['33001', 'emily brown', 26.0, ['BEAUTY', 'HOME', 'FOOD'], [213, 659, 79]],
+    ['33767', ' Maria Garcia', 33.0, ['CLOTHES', 'FOOD', 'BEAUTY'], [499, 189, 63]],
+    ['33912', 'JOSE MARTINEZ', 22.0, ['SPORT', 'ELECTRONICS', 'HOME'], [259, 549, 109]],
+    ['34009', 'lisa wilson ', 35.0, ['HOME', 'BOOKS', 'CLOTHES'], [329, 189, 329]],
+    ['34278', 'James Lee', 28.0, ['BEAUTY', 'CLOTHES', 'ELECTRONICS'], [189, 299, 579]],
+]
+
+name_index = 1
+age_index = 2
+cat_index = 3
+users_cleaned = []
+
+for user in users: # escribe tu código aquí
+    user_cleaned = clean_user(user, name_index, age_index, cat_index) # escribe tu código aquí
+    users_cleaned.append(user_cleaned) # escribe tu código aquí
+
+print(users_cleaned)
+
+
+
+print()
+print()
+
+users = [['32415', ['mike', 'reed'], 32, ['electronics', 'sport', 'books'], [894, 213, 173]],
+         ['31980', ['kate', 'morgan'], 24, ['clothes', 'books'], [439, 390]],
+         ['32156', ['john', 'doe'], 37, ['electronics', 'home', 'food'], [459, 120, 99]],
+         ['32761', ['samantha', 'smith'], 29, ['clothes', 'electronics', 'beauty'], [299, 679, 85]],
+         ['32984', ['david', 'white'], 41, ['books', 'home', 'sport'], [234, 329, 243]],
+         ['33001', ['emily', 'brown'], 26, ['beauty', 'home', 'food'], [213, 659, 79]],
+         ['33767', ['maria', 'garcia'], 33, ['clothes', 'food', 'beauty'], [499, 189, 63]],
+         ['33912', ['jose', 'martinez'], 22, ['sport', 'electronics', 'home'], [259, 549, 109]],
+         ['34009', ['lisa', 'wilson'], 35, ['home', 'books', 'clothes'], [329, 189, 329]],
+         ['34278', ['james', 'lee'], 28, ['beauty', 'clothes', 'electronics'], [189, 299, 579]]]
+
+revenue = 0
+
+for user in users:
+	# escribe tu código aquí
+    spending_list = user[4]
+    revenue += sum(spending_list)
+
+print(revenue)
+
+
+from random import randint
+
+total_amount_spent = 1280
+target_amount = 1500
+
+while total_amount_spent < target_amount: # escribe tu código aquí
+	new_purchase = randint(30, 80) # generamos un número aleatorio de 30 a 80
+	total_amount_spent +=  new_purchase# escribe tu código aquí
+
+print(total_amount_spent)
+
+
+users = [['32415', ['mike', 'reed'], 32, ['electronics', 'sport', 'books'], [894, 213, 173]],
+         ['31980', ['kate', 'morgan'], 24, ['clothes', 'books'], [439, 390]],
+         ['32156', ['john', 'doe'], 37, ['electronics', 'home', 'food'], [459, 120, 99]],
+         ['32761', ['samantha', 'smith'], 29, ['clothes', 'electronics', 'beauty'], [299, 679, 85]],
+         ['32984', ['david', 'white'], 41, ['books', 'home', 'sport'], [234, 329, 243]],
+         ['33001', ['emily', 'brown'], 26, ['beauty', 'home', 'food'], [213, 659, 79]],
+         ['33767', ['maria', 'garcia'], 33, ['clothes', 'food', 'beauty'], [499, 189, 63]],
+         ['33912', ['jose', 'martinez'], 22, ['sport', 'electronics', 'home'], [259, 549, 109]],
+         ['34009', ['lisa', 'wilson'], 35, ['home', 'books', 'clothes'], [329, 189, 329]],
+         ['34278', ['james', 'lee'], 28, ['beauty', 'clothes', 'electronics'], [189, 299, 579]]]
+
+
+# escribe tu código aquí
+for user in users:
+    if user[2] < 30:
+        print(user[1][0])
+
+print()
+for user in users:
+    if user[2] < 30 and sum(user[4]) > 1000:
+        print(user[1][0])
+
+print()
+for user in users:
+    for category in user[3]:
+        if category == 'clothes':
+            print(user[1][0], user[2])
+
+print()
+print()
+def get_client_by_cat(users, id_index, name_index, age_index, category_index, amounts_index, filter_category):
+    # escribe tu código aquí
+    filtered_clients = []
+    for user in users:
+        for category in user[category_index]:
+            if category == filter_category:
+                id_user = user[id_index]
+                name_user = user[name_index]
+                age_user = user[age_index]
+                expenses_user = sum(user[amounts_index])
+                filtered_clients.append([id_user, name_user, age_user, expenses_user])
+    
+    return filtered_clients
+
+# La lista de usuarios
+users = [
+    ['32415', ['mike', 'reed'], 32, ['electronics', 'sport', 'books'], [894, 213, 173]],
+    ['31980', ['kate', 'morgan'], 24, ['clothes', 'books'], [439, 390]],
+    ['32156', ['john', 'doe'], 37, ['electronics', 'home', 'food'], [459, 120, 99]],
+    ['32761', ['samantha', 'smith'], 29, ['clothes', 'electronics', 'beauty'], [299, 679, 85]],
+    ['32984', ['david', 'white'], 41, ['books', 'home', 'sport'], [234, 329, 243]],
+    ['33001', ['emily', 'brown'], 26, ['beauty', 'home', 'food'], [213, 659, 79]],
+    ['33767', ['maria', 'garcia'], 33, ['clothes', 'food', 'beauty'], [499, 189, 63]],
+    ['33912', ['jose', 'martinez'], 22, ['sport', 'electronics', 'home'], [259, 549, 109]],
+    ['34009', ['lisa', 'wilson'], 35, ['home', 'books', 'clothes'], [329, 189, 329]],
+    ['34278', ['james', 'lee'], 28, ['beauty', 'clothes', 'electronics'], [189, 299, 579]]
+]
+
+# Llama a la función con la categoría 'home'
+result = get_client_by_cat(users, 0, 1, 2, 3, 4, 'home') # escribe tu código aquí
+
+# Muestra en pantalla la lista que resulta
+print(result)
