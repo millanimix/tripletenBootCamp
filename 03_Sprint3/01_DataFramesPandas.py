2ffa89448d71a73e85457d992372f15d624de78e
Dr. Big Gorilla <millan.galeis@gmail.com>
2024-12-18 21:09:46 -0600

    Logic filters

--- 03_Sprint3/01_DataFramesPandas.py
+++ 03_Sprint3/01_DataFramesPandas.py
@@ -58,7 +58,7 @@ print()
 print('Read CSV and Excel files')
 print()
 
-df = pd.read_excel('03_Sprint3/datasets/music.xlsx')
+# df = pd.read_excel('03_Sprint3/datasets/music.xlsx')
 
 df = pd.read_csv('03_Sprint3/datasets/music.csv')
 df.info()
@@ -68,3 +68,39 @@ print(df.tail(3))
 print()
 print('Indexación de un Dataframe')
 print()
+
+# Filtrado con indexación lógica
+
+print()
+print('Filtrado con indexación lógica')
+print()
+
+print(df['genre'] == 'pop')
+
+print(df.loc[df['genre'] == 'pop'])
+
+df = df[df['total play'] >= 80]
+df = df[df['total play'] <= 130]
+df = df[df['genre'] == 'jazz']
+
+
+print(df)
+
+print()
+print()
+
+data = {
+    'sucursal': ['Centro', 'Norte', 'Sur', 'Centro', 'Norte'],
+    'producto': ['Camiseta roja', 'Zapatillas azules', 'Camisa blanca', 'Jeans negro', 'Camiseta roja'],
+    'cliente': ['Ana Garcia', 'Luis Rodriguez', 'Pedro Hernandez', 'Maria Lopez', 'Juan Perez'],
+    'cantidad': [2, 1, 3, 1, 4],
+    'precio_unitario': [15.99, 49.99, 29.99, 39.99, 15.99],
+    'venta_total': [31.98, 49.99, 89.97, 39.99, 63.96]
+}
+
+df = pd.DataFrame(data)
+
+filtered_data = df.loc[((df.loc[:,'sucursal'] == 'Centro') | (df.loc[:,'sucursal'] == 'Norte')) & (df.loc[:,'precio_unitario'] > 25)]
+
+print(filtered_data)
+
