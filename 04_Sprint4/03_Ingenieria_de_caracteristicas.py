c2bfaab889bd8a024389cc7bae8e191372e4bd1c
Dr. Big Gorilla <millan.galeis@gmail.com>
2025-03-06 18:32:51 -0600

    Create New Categorical columns using apply() method

--- 04_Sprint4/03_Ingenieria_de_caracteristicas.py
+++ 04_Sprint4/03_Ingenieria_de_caracteristicas.py
@@ -1,6 +1,7 @@
 # Ingenieria_de_caracteristicas
 
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('04_Sprint4/datasets/vg_sales.csv')
 print(df.head())
@@ -35,3 +36,41 @@ df = pd.read_csv('04_Sprint4/datasets/vg_sales.csv')
 df['average_sales'] = (df['na_sales'] + df['eu_sales'] + df['jp_sales']) / 3
 df = df.sort_values(by='average_sales', ascending=False)
 print(df.head())
+
+print('Create categorical columns with apply')
+df = pd.read_csv('04_Sprint4/datasets/vg_sales.csv')
+print(df['year_of_release'].min(), df['year_of_release'].max())
+# Games by year
+df_year_of_release = df['year_of_release'].value_counts().sort_index()
+print(df_year_of_release)
+
+def era_group(year):
+    """
+    Returns the era of the gieven year
+    -'retro' < 2000
+    -'modern' >= 2000 year < 2010
+    -'recent' >= 2010
+    -'unknown' if year is NaN
+    """
+
+    if year < 2000:
+        return 'retro'
+    elif year < 2010:
+        return 'modern'
+    elif year >= 2010:
+        return 'recent'
+    else:
+        return 'unknown'
+    
+print(era_group(1983))
+print(era_group(2009))
+print(era_group(2021))
+print(era_group(np.nan))
+
+print('Aply() method')
+df['era_group'] = df['year_of_release'].apply(era_group)
+print(df.head())
+
+print(df['era_group'].value_counts())
+
+
